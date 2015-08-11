293281e67b80fbf3b8dfb8e4a7b056939501c680
pat <patrick.h.needham@gmail.com>
2015-08-10 23:02:14 -0400

    first commit - yellow background, black nav, here we go

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// cheers to http://tympanus.net/TipsTricks/DirectionAwareHoverEffect/
+console.log('If you can read this, you are probably a web developer :)');
+
+$('h1').click(function(){
+  console.log('that\'s my name!');
+});
